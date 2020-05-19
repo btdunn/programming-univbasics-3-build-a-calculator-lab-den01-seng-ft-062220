15a781af8d63e2dc5b0cc1146b93d34850ec311f
interactive-directory-1839 <benthedunn@gmail.com>
2020-05-19 23:12:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,16 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  sum
 end
 
 def multiplication(num1, num2)
